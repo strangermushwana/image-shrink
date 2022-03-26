21cb26c1494062b2b0fbab77a81ab617624f4687
Stranger Mushwana <strangermushwana@gmail.com>
2022-03-26 22:40:20 +0200

    setup nodemom and index page

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const { app, BrowserWindow }  = require('electron')
 
+process.env.NODE_ENV = 'development'
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'darwin' ? true : false
+let mainWindow
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
-    height: 600
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
   })
+
+  mainWindow.loadFile('./app/index.html')
 }
 
 app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow()
+  }
+})
